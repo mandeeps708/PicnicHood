554de468348d0594ded5d1540c59e18ab9974ee4
Mandeep Singh <mandeeps708@gmail.com>
2025-04-23 23:08:02 +0000

    fix(docs): fix api docs

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.API_URL || 'http://localhost:3000',
+        // url: process.env.API_URL || 'http://localhost:3000',
         description: process.env.NODE_ENV === 'production' ? 'Production server' : 'Development server',
       },
     ],
