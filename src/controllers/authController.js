e17f7018f36b182086f6b8dc582dc5f944c2998e
Mandeep Singh <mandeeps708@gmail.com>
2025-04-24 07:27:14 +0200

    feat(community): return community id with user login data

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -72,7 +72,8 @@ const login = async (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
-        role: user.role
+        role: user.role,
+        community: user.community
       }
     });
   } catch (error) {
